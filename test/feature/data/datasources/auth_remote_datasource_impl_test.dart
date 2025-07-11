a721f5c431bd9f5335c56c56c2b6718938b0b5c1
Palash Shrivastava <palash.shrivastava@artivatic.ai>
2025-07-11 12:07:27 +0530

    added test case

--- test/feature/data/datasources/auth_remote_datasource_impl_test.dart
+++ test/feature/data/datasources/auth_remote_datasource_impl_test.dart
@@ -4,6 +4,7 @@ import 'package:mocktail/mocktail.dart';
 import 'package:serendipity/core/utils/constants.dart';
 import 'package:serendipity/features/auth/data/datasources/auth_remote_datasource.dart';
 import 'package:serendipity/features/auth/data/datasources/auth_remote_datasource_impl.dart';
+import 'package:serendipity/features/auth/data/models/response_models/questionnaire_response_model/questionnaire_response_model.dart';
 
 import '../../../core/mocks/mock_classes.dart';
 
@@ -62,5 +63,38 @@ void main(){
       final data = authRemoteDatasource.register('test', 'test');
       expect(()=>data, throwsA(isA<Exception>()));
     });
+
+    test('Should return success response from Questionnaire API and parse data into QuestionnaireResponseModel from raw data', ()async{
+    final mockResponse = {
+      'data': [
+        {
+          'id':1,
+          'Whats your name': 'Palash',
+        }
+      ]
+    };
+
+    final responseModel = QuestionnaireResponseModel.fromJson(mockResponse);
+
+    when(() => mockDioClient.get(any())).thenAnswer((_)async=> mockResponse);
+
+    final data = await authRemoteDatasource.getQuestionnaire();
+
+    expect(data.questionnaires?.first.id, responseModel.questionnaires?.first.id);
+
   });
+
+  test('Should throw error if API call fails', (){
+    when(() => mockDioClient.get(any())).thenThrow(Exception('API Failed'));
+
+    final data = authRemoteDatasource.getQuestionnaire();
+
+    expect(()=> data, throwsA(isA<Exception>()));
+
+  });
+    
+  });
+
+
+  
 }
